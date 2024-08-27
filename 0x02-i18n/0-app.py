f16084b9b3b5da1fbcbc454f4b38bea08d00b621
vintage254 <derricknjuguna414@gmail.com>
2024-08-27 09:40:03 -0700

    Basic Flask app fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""" 0-app module """
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
+    """Route for the home page"""
     return render_template('0-index.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
