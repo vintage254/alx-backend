d317f134b3b7f172af00d74f7ce057c036ddcaae
vintage254 <derricknjuguna414@gmail.com>
2024-09-25 07:09:19 +0000

    sync and async redis clinet functions

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import redis from "redis";
+import { promisify } from "util";
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on("connect", ()=> {
+        console.log("Redis client connected to the server");
+})
+
+
+client.on("error", (err)=> {
+        console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+
+function setNewSchool( schoolName, value){
+        client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue( schoolName){
+	try{
+		const reply = await getAsync(schoolName);
+		console.log(reply);
+	}catch (err) {
+		console.error(err);
+	}
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
